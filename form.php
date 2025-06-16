e795db0670939ba0e4908c5a009b2638f05aa808
gutti-misdo <2301162@s.asojuku.ac.jp>
2025-06-16 10:07:15 +0900

    y

--- form.php
+++ form.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,11 +11,12 @@
 <body>
     <h1>💬 一言掲示板</h1>
     <form action="post.php" method="post">
-        <p>名前：<input type="text" name="name" required></p>
+        <p>ようこそ、<?php echo $_SESSION['user']['username']; ?>さん</p>
         <p>コメント：<br>
         <textarea name="comment" rows="4" cols="40" required></textarea></p>
         <p><button type="submit">投稿する</button></p>
     </form>
     <p><a href="view.php">▶ 投稿一覧を見る</a></p>
+    <p><a href="logout.php">ログアウト</a></p>
 </body>
 </html>
---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+?>
+<?php
+unset($_SESSION['user']);
+header('Location: login.php');
+?>
