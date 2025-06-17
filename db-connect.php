9ed710a37e31e962adce5b016f46cbc79705de31
gutti-misdo <2301162@s.asojuku.ac.jp>
2025-06-18 00:00:02 +0900

    commprete

--- db-connect.php
+++ db-connect.php
@@ -1,3 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=short_bbs;charset=utf8mb4','root','');
+//$pdo = new PDO('mysql:host=localhost;dbname=shortbbs;charset=utf8mb4','root','');
+$pdo = new PDO('mysql:host=mysql320.phy.lolipop.lan;dbname=LAA1553900-shortbbs;charset=utf8', 'LAA1553900', 'Pass1105');
 ?>
